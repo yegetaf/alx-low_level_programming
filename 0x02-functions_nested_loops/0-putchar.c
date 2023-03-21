5dc540a5b6c9a186cf6dcc385a14a25300f66ec9
yegetaf <ygyegeta@gmail.com>
2023-03-21 15:19:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**                                                                                                                                        \
+*main                                                                                                                                      
+*for loop for each char                                                                                                                    
+*print each char                                                                                                                           
+**/
+int main(void)
+{
+char arr[]="_putchar";
+for (int i=0; i<7; i++){
+putchar(arr[i]);
+ putchar('\n');
+}
+
+return (0);
+}
